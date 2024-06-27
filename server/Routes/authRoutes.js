5e420919b1cd865007f38e1c32d6b28bb6e4aed5
Saurav Suman <sauravsrivastav9876@gmail.com>
2024-06-27 12:02:10 +0530

    register

--- server/Routes/authRoutes.js
+++ server/Routes/authRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const user = require('../models/userModel');
 const session = require('express-session');
+const cors = require('cors');
+const app = express();
 const mongodbSession = require('connect-mongodb-session')(session);
 const bcrypt = require('bcrypt');
 
@@ -20,28 +22,41 @@ router.use(session({
     saveUninitialized: false,
     store: store
 }))
-const isAuth = (req,res,next)=>{
+const isAuthe = (req,res,next)=>{
+    console.log(req.session.isAuth)
     if(req.session.isAuth){
+        console.log("hyy")
         next();
     }else{
-        res.send({authentication:req.session.isAuth});
+        console.log("ghjh")
+        let val = req.session.isAuth;
+        res.send({authentication:val});
     }
 }
-router.post('/register', async(req,res)=>{
-    
-    // const salt = await bcrypt.genSalt(10);
-    // var secPassword = await bcrypt.hash(req.body.password, salt)
-    // let userdet = await user.create({
-    //    name: req.body.name,
-    //    password: secPassword,
-    //    email: req.body.email,
-       
-    // })
-    // res.send({message:"User created successfully"})
-    // // const {name, email, password} = req.body;
-    
-});
+router.post('/register', async (req,res)=>{
+    const { name, email, password } = req.body;
 
+    if (!name || !email || !password) {
+        res.send("All fields are required");
+    }
+
+    const existingUser = await user.findOne({ email });
+    if (existingUser) {
+        res.send("Email already in use");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new user({
+        name,
+        email,
+        password:hashedPassword
+    });
+
+    await newUser.save();
+
+    res.send("User registered successfully");
+});
 router.post('/login',async(req,res)=>{
     const {email,password} = req.body;
     const avilUser =await  user.findOne({email})
@@ -55,9 +70,13 @@ if(!isMatch){
        return res.send({authentication:false})
        
     }
+    req.session.isAuth = true;
     return res.send({authentication:true})
     
 });
+router.post('/dashboard',isAuthe,(req,res)=>{
 
+    res.send({message:"Welcome to dashboard"})
+})
 
 module.exports = router;
