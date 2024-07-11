428a995e9a39691e14d4a8c5c984bee7f7ac8dee
Sushant <sushantsingh408sks@gmail.com>
2024-07-11 06:45:36 +0530

    profile updated

--- server/Routes/atsRoutes.js
+++ server/Routes/atsRoutes.js
@@ -1,8 +1,50 @@
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
+const filePath =".././client/src/resume_txt/";
+router.post('/getResume', (req,res)=>{
+   try{
+    let file =filePath+req.body.name;
+    // return res.send(file)
+   const data= fs.readFileSync(file,'utf8');
 
-router.post('/validateResume', (req,res)=>{
-    res.send("Validating user resume")
+   try{
+    
+    const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+    // Access your API key as an environment variable (see "Set up your API key" above)
+    const genAI = new GoogleGenerativeAI("AIzaSyAhbZ3uop8Qp0r2TtpkG3ldRK06yminNj8");
+    
+    async function run() {
+      // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+    
+      const prompt = `${data} use this resume to generate `;
+    
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      let cleanedString = text.replace(/```javascript|```/g, '');
+      cleanedString = cleanedString.replace("json","")
+      const questionsArray = JSON.parse(cleanedString);
+// console.log(tes;
+      res.send(questionsArray);
+    }
+    
+    run();
+   }catch(err){
+    res.send({error: "error occured in reading file.txt"});
+   }
+
+//    res.send(data);
+   
+   }catch(err){
+    res.send({error: "error occured in reading file.txt"});
+   }
+});
+
+router.get('/:userId',(req,res)=>{
+    res.send("User result");
 });
 
 module.exports = router;
