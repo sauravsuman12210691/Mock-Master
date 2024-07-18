6f21feff1c93f29f4fc7b678ea15d43ae555e19b
Saurav Suman <117572833+sauravsuman12210691@users.noreply.github.com>
2024-07-18 16:36:28 +0530

    Update mockInterviewRoutes.js

--- server/Routes/mockInterviewRoutes.js
+++ server/Routes/mockInterviewRoutes.js
@@ -5,17 +5,17 @@ const router = express.Router();
 const dotenv = require('dotenv');
 dotenv.config();
 
-const filePath = path.join(__dirname, '../client/src/resume_txt/');
+const filePath = path.join(__dirname, '../resume_txt/');
 
 router.post('/get-questionArray', async (req, res) => {
-    try {
+   
         const fileName = req.body.name;
         const fullFilePath = path.join(filePath, fileName);
         const data = fs.readFileSync(fullFilePath, 'utf8');
 
         const { GoogleGenerativeAI } = require('@google/generative-ai');
 
-        const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+        const genAI = new GoogleGenerativeAI("AIzaSyCrz8IzEzcdfZiWMlb-8fK8fkpDaNh-IVE");
 
         const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
@@ -30,10 +30,12 @@ router.post('/get-questionArray', async (req, res) => {
         const questionsArray = JSON.parse(cleanedString);
         
         res.send({ questionsArray });
-    } catch (err) {
-        res.send({ error: "An error occurred while generating questions." });
-    }
-});
+})
+    
+//     } catch (err) {
+//         res.send({ error:err });
+//     }
+// });
 
 router.get('/:userId', (req, res) => {
     res.send("User result");
